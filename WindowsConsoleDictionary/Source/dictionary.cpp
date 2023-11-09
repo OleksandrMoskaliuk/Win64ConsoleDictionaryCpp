dc08cb684513527a58b051697ab58d3de5eb36d8
AlexMercer <redownloader999@gmail.com>
2023-11-09 10:58:18 +0200

    MOre words + lodger time delay test

--- WindowsConsoleDictionary/Source/dictionary.cpp
+++ WindowsConsoleDictionary/Source/dictionary.cpp
@@ -879,7 +879,7 @@ void my_dictionary::MyDictionary::tets_yourself_word_checker(
   }
   generate = true;
   print_once = true;
-  std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 }
 
 
